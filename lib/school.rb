b3dd47ca96d51eaa79a767e1d6ae1a4886043122
Eugene Theriault <hybridbassist@gmail.com>
2020-08-19 12:16:33 -0700

    Complete iteration 2

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,13 @@ class School
     @hours_in_school_day = hours_in_school_day
     @student_names = []
   end
-  
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    first_digit = @start_time[0].to_i + @hours_in_school_day
+    "#{first_digit}:00"
+  end
 end
